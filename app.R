03a6d431d0d441d8b618842a06346f015cc3d4a6
Jasmine Ortega <jorteg01@ubc.ca>
2022-03-14 13:23:10 -0700

    add first plot

--- app.R
+++ app.R
@@ -1,3 +1,5 @@
+# add theme
+
 library(dplyr)
 library(tidyverse)
 library(readr)
@@ -8,7 +10,7 @@ library(plotly)
 library(dash)
 library(purrr)
 
-j_data <- read_csv("data/jasmine_df.csv")
+j_data <- read_csv("data/processed/jasmine_df.csv")
 
 j_data <- j_data %>% filter(is.na(j_data$cast_count) == FALSE)
 
@@ -20,25 +22,77 @@ app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
 
 app$layout(
-  dbcContainer(
-    list(
-      dccGraph(id='scatter'),
-      htmlLabel('Year Range'),
-      dccSlider(
-        id='xslider',
-        min=1942,
-        max=2019,
-        marks = list(
-          '1942' = '1942',
-          '1962' = '1962',
-          '1982' = '1982',
-          '2002' = '2002',
-          '2019'= '2019'
-        ),
-        value=2002 # add start range at 1942 
-      )
-    )
-  )
+  dbcContainer(list(
+    
+    # title card 
+    dbcRow(
+      dbcCol(
+        htmlDiv("Netflix Movie Dashboard: Visualize movie trends on the world's most popular streaming platform!")
+      ),
+    ),
+    
+    # first row
+    dbcRow(
+      
+      # Jasmine column
+      dbcCol(
+        dbcCard(
+          dbcCardBody(
+            htmlDiv(
+              list(
+                dccGraph(id='scatter'),
+                htmlLabel('Year Range'),
+                dccSlider(
+                  id='xslider',
+                  min=1942,
+                  max=2019,
+                  marks = list(
+                    '1942' = '1942',
+                    '1962' = '1962',
+                    '1982' = '1982',
+                    '2002' = '2002',
+                    '2019'= '2019'
+                  ),
+                  value=2002 
+                )
+              )
+            ) # html 
+          ) # card body 
+        ) # card
+        ,
+        md=6), # first column
+      
+      # Masha column 
+      dbcCol(
+        dbcCard(
+          dbcCardBody(
+            htmlDiv(
+              list(
+                htmlLabel('Is this working?')
+              ) # list
+            ) # htmlDiv
+          ) # card body 
+        ), # card
+        
+        md=6) # second column
+      
+    ), # second row
+    
+    # Sufang card
+    dbcRow(
+      dbcCard(
+        dbcCardBody(
+          htmlDiv(
+            list(
+              htmlLabel('Sufang plot')
+            ) # list
+          ) # htmlDiv
+        ) # CardBody 
+      ) # card
+    ) # second row 
+    
+  ) # list 
+  ) # container  
 )
 
 app$callback(
@@ -49,19 +103,18 @@ app$callback(
     p <- cast_data %>% 
       ggplot(aes(x=release_year,
                  y=mean_cast_count)) + 
-      geom_point() +
+      geom_point(color= "red2") +
       ggtitle("Average Cast Size Per Year") +
       xlab("Release Year") +
       ylab("Avg. Cast Size") +
-      xlim(1942, xcol)
+      xlim(1942, xcol) + 
+      theme(plot.title = element_text(hjust = 0.5),
+            panel.background = element_rect(size = 0.5, linetype = "solid"),
+      )
     ggplotly(p+ aes(text = release_year), tooltip = 'release_year')
     
   }
 )
 
-
-# app$run_server(debug = T)
-
 app$run_server(host = '0.0.0.0')
 # app$run_server(debug = T) # use when running locally
-
